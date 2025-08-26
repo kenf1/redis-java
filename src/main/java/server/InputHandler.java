401b6dd5492ae00a780e04dc013d7065ff8e87f3
kenf1 <103538771+kenf1@users.noreply.github.com>
2025-08-26 09:33:37 -0700

    Add expiry

--- src/main/java/server/InputHandler.java
+++ src/main/java/server/InputHandler.java
@@ -7,13 +7,37 @@ import java.util.Map;
 
 public class InputHandler {
     public final Map<String, String> db;
+    public final Map<String,Long> expiryDB;
     private final int port;
     private volatile boolean listening;
 
-    public InputHandler(Map<String, String> db, int port) {
+    public InputHandler(Map<String, String> db,Map<String,Long> expiryDB, int port) {
         this.db = db;
+        this.expiryDB = expiryDB;
         this.port = port;
         this.listening = true;
+    }
+
+    private static void checkEmptyInput(BufferedWriter clientOutput, String val) throws IOException {
+        if (val != null) {
+            clientOutput.write("$" + val.length() + "\r\n" + val + "\r\n");
+        } else {
+            clientOutput.write("$-1\r\n");
+        }
+    }
+
+    public boolean setExpiry(BufferedWriter clientOutput,String[] args) throws IOException{
+        if (args.length >= 5 && "px".equalsIgnoreCase(args[3]) && args[4] != null) {
+            try {
+                long pxMillis = Long.parseLong(args[4]);
+                expiryDB.put(args[1], System.currentTimeMillis() + pxMillis);
+            } catch (NumberFormatException e) {
+                clientOutput.write("-ERR PX value is not a valid integer\r\n");
+                clientOutput.flush();
+                return true;
+            }
+        }
+        return false;
     }
 
     public Map<String, String> db() {
@@ -94,6 +118,25 @@ public class InputHandler {
             case "set":
                 if (args.length >= 3 && args[1] != null && args[2] != null) {
                     db.put(args[1], args[2]);
+
+                    if (setExpiry(clientOutput,args)) break;
+
+                    String key = args[1];
+                    if(expiryDB.containsKey(key)){
+                        long expiryTime = expiryDB.get(key);
+
+                        if(System.currentTimeMillis() > expiryTime){
+                            expiryDB.remove(key);
+                            db.remove(key);
+
+                            clientOutput.write("$-1\r\n");
+                            clientOutput.flush();
+                        }
+                    }
+
+                    String val = db.get(args[1]);
+                    checkEmptyInput(clientOutput,val);
+
                     clientOutput.write("+OK\r\n");
                 } else {
                     clientOutput.write("-ERR wrong number of arguments for 'set' command\r\n");
@@ -103,11 +146,7 @@ public class InputHandler {
             case "get":
                 if (args.length >= 2 && args[1] != null) {
                     String val = db.get(args[1]);
-                    if (val != null) {
-                        clientOutput.write("$" + val.length() + "\r\n" + val + "\r\n");
-                    } else {
-                        clientOutput.write("$-1\r\n");
-                    }
+                    checkEmptyInput(clientOutput, val);
                 } else {
                     clientOutput.write("-ERR wrong number of arguments for 'get' command\r\n");
                 }
